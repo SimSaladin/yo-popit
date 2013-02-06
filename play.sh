c129f8c71d681234feed721fa7c436007abd0459
Samuli Thomasson <simsaladin@paivola.fi>
2013-02-06 17:01:31 +0200

    some settings

--- play.sh
+++ play.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 mpc clear
+
 mpc load $1
+
+# set some settings
 mpc random off
 mpc single off
 mpc repeat off
 mpc consume on
+mpc crossfade 3
+mpc replaygain off
+
 mpc enable 1
 
 sleep 2 && mpc play
